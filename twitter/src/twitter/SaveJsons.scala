c057dba4b8842f5e43215906088d50cb2f928c53
Kathrin Netzer <k.netzer@reliatec.de>
2017-01-30 12:26:38 +0100

    twitter

---
+++ twitter/src/twitter/SaveJsons.scala
@@ -0,0 +1,47 @@
+package twitter
+
+import java.io._
+import twitter4j._
+import scala.io.Source
+import com.google.gson._
+
+/**
+  * Created by KathrinNetzer on 29.01.2017.
+  */
+class SaveJsons(path: String) {
+  val jsonFile = new File(path)
+  val gsonFormatter = new GsonBuilder().setPrettyPrinting().create
+  val gsonParser = new JsonParser()
+
+  def saveJsonString(jString: String): Unit = {
+
+    var sString = jString
+    val parsedStatementString = gsonParser.parse(sString)
+    sString = gsonFormatter.toJson(parsedStatementString)
+    val bufferedJsonWriter = new BufferedWriter(new FileWriter(jsonFile))
+    bufferedJsonWriter.write(jString)
+    bufferedJsonWriter.close()
+  }
+
+  /**
+    *
+    * @param statementID id of the statement the comment refers to
+    * @param commenttext Comment text
+    * @param username Screenname of the user that commented
+    * @param likes [User Screenname]
+    */
+  def saveKomment(statementID: Int, commenttext: String, username: String, likes: Array[String]): Unit ={
+    val commentsJsonString =  Source.fromFile(path).getLines.mkString
+    val commentsJson = new JSONObject(commentsJsonString)
+    val comments: JSONArray = commentsJson.getJSONArray("data")
+
+    val commentJson: JSONObject = new JSONObject()
+    commentJson.put("type", "comment")
+    commentJson.put("message", commenttext)
+    commentJson.put("screenname", username)
+    commentJson.put("likes", likes)
+    comments.put(commentJson)
+    val commentssString = "{\"data\": " + comments.toString() + '}'
+    saveJsonString(commentssString)
+  }
+}
---
+++ twitter/src/twitter/scala.scala
@@ -0,0 +1,102 @@
+package twitter
+
+import twitter4j._
+import twitter4j.TwitterStreamFactory
+
+import scala.collection.mutable.Queue
+import scala.io.Source
+
+
+/**
+  * @author Kathrin Netzer
+  */
+
+// import scala.collection.mutable.ArrayBuffer
+
+class TwitterAccess {
+
+  // Put statements in a queue for the view
+  var _statementsQueue = new Queue[JSONObject]
+  def statementsQueue = this._statementsQueue
+  def setStatementsQueue(twitterJson: JSONObject): Unit = {
+    this._statementsQueue += twitterJson
+  }
+
+  // Settings for the registered application
+  val TwitterSettings = new TwitterSettings
+  val settings = TwitterSettings.settings
+
+  // Statements should be saved in JSON, done in classe SaveJsons
+  val StatementsPath = TwitterSettings.pathToStatements
+  val tweetsJsonString = Source.fromFile(StatementsPath).getLines.mkString
+  val tweetsJson = new JSONObject(tweetsJsonString)
+  val statements: JSONArray = tweetsJson.getJSONArray("data")
+  val StatementsSaver = new SaveJsons(StatementsPath)
+
+
+  val streamFactory = new TwitterStreamFactory(settings.build()).getInstance
+  //streamFactory.addListener(getPosts(statements))
+  streamFactory.addListener(getPosts())
+  val politicalFilter = new FilterQuery()
+
+  //  follow - Specifies the users, by ID, to receive public tweets from.
+  politicalFilter.follow()
+  politicalFilter.track(TwitterSettings.hashtags)
+  politicalFilter.filterLevel(TwitterSettings.filterLevel)
+  politicalFilter.language(TwitterSettings.filterLanguage)
+  streamFactory.filter(politicalFilter)
+
+  /**
+    * Twitter Stream:
+    * The Thread is important, because the Stream has to be running all the time.
+    */
+
+  val streamingThread = new Thread(new Runnable {
+    override def run(): Unit = {
+      // in periodical time intervals:  update json file
+      var counter = 0
+      while (!Thread.currentThread().isInterrupted()) {
+        if(counter < 3){
+          counter += 1
+        }else{
+          counter = 0
+          // update Json File: Write statements to file
+          val statementsString = "{\"data\": " + statements.toString() + '}'
+          StatementsSaver.saveJsonString(statementsString)
+        }
+
+        Thread.sleep(10000) // important: not too low!
+        streamFactory.cleanUp()
+        streamFactory.shutdown()
+    }}
+  })
+  streamingThread.start()
+
+  //def getPosts(statements: JSONArray) = new StatusListener() {
+
+  def getPosts() = new StatusListener {
+    def onStatus(status: Status) {
+      // Create Json from tweetJson
+      val tweetJson:JSONObject = new JSONObject()
+      tweetJson.put("type", "statement")
+      tweetJson.put("message", status.getText)
+      tweetJson.put("userid", status.getUser.getId)
+      tweetJson.put("name", status.getUser.getName)
+      tweetJson.put("screenname", status.getUser.getScreenName)
+      tweetJson.put("pictureurl", status.getUser.getProfileImageURL)
+      tweetJson.put("created_at", status.getCreatedAt.toString)
+      tweetJson.put("id", status.getId)
+      // put Json Statement into the Queue
+      setStatementsQueue(tweetJson)
+      statements.put(tweetJson)
+
+    }
+
+    def onDeletionNotice(statusDeletionNotice: StatusDeletionNotice) {}
+    def onTrackLimitationNotice(numberOfLimitedStatuses: Int) {}
+    def onException(ex: Exception) { ex.printStackTrace }
+    def onScrubGeo(arg0: Long, arg1: Long) {}
+    def onStallWarning(warning: StallWarning) {}
+  }
+
+}
