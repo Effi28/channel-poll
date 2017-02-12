61ff5308263d27690e6d6cd1ada92fa3e3067448
Michael Fromm <fromm@cip.ifi.lmu.de>
2017-02-12 15:08:03 +0100

    bugfix

--- channel-poll1/src/main/shared/data/TwitterUser.scala
+++ channel-poll1/src/main/shared/data/TwitterUser.scala
@@ -3,7 +3,7 @@ package main.shared.data
 /**
   * Created by KathrinNetzer on 05.02.2017.
   */
-final class TwitterUser(val ID: Long, val userName: String) {
+case class TwitterUser(val ID: Long, val userName: String) {
 
   override def toString: String = userName
 
