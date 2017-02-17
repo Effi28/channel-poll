178ecddf077e2f1b1ff0d12c86d8a717cfdd260c
Brenda <n.avdijaj@reliatec.de>
2017-02-18 00:20:24 +0100

    neue login view

--- channel-poll1/src/main/client/view/LoginView.scala
+++ channel-poll1/src/main/client/view/LoginView.scala
@@ -5,12 +5,16 @@ package main.client.view
 
 import java.awt.Desktop
 import main.client.controller.Controller
-import scalafx.application.{JFXApp}
+import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.scene.Scene
 import scalafx.scene.control.{Button, TextField}
-import scalafx.scene.layout.{BorderPane, VBox}
-
+import scalafx.scene.layout.BorderPane
+import scalafx.scene.paint.Color._
+import scalafx.geometry.{Insets, Pos}
+import scalafx.scene.control.Label
+import scalafx.scene.layout.GridPane
+import scalafx.scene.text.Text
 /**
   * Created by Brenda on 30.01.17.
   */
@@ -18,12 +22,29 @@ import scalafx.scene.layout.{BorderPane, VBox}
 final object LoginView extends JFXApp {
   stage = new PrimaryStage {
     title = "Login"
-    height = 300
-    width = 300
+
     scene = new Scene {
-      //stylesheets = List(getClass.getResource("material-fx-v0_3.css").toExternalForm)
+
+
+      //stylesheets = List(getClass.getResource("flatterfx.css").toExternalForm)
       val border = new BorderPane()
+
+      val grid = new GridPane()
+
+      grid.vgap = 10
+      grid.hgap = 10
+
+      grid.margin = Insets(10)
+
+      val welcomeMessage = new Label("Welcome to ChannelPoll")
+      welcomeMessage.maxWidth = Double.MaxValue
+      welcomeMessage.alignment = Pos.Center
+      welcomeMessage.style = "-fx-font-size: 18pt;"
+      grid.addRow(0, welcomeMessage)
+
       val loginButton = new Button("Login")
+      loginButton.maxWidth = Double.MaxValue
+      loginButton.alignment = Pos.Center
       loginButton.onAction = e => {
         val loginURL = TwitterLogin.startLogin()
         if (Desktop.isDesktopSupported) {
@@ -31,31 +52,51 @@ final object LoginView extends JFXApp {
         } else {
           println(loginURL)
         }
-
-
-
-
+        loginButton.visible = false
+        codeField.visible = true
+        submitButton.visible = true
       }
-      val codeTextField = new TextField()
-      codeTextField.promptText = "Enter your code"
+
+      grid.addRow(1, loginButton)
+
+
+      val codeField = new TextField()
+      codeField.promptText = "Enter your code..."
+      codeField.maxWidth = Double.MaxValue
+      codeField.alignment = Pos.Center
+      codeField.visible = false
+      grid.addRow(2, codeField)
+
+
       val submitButton = new Button("Submit")
-      /**
-        * Hier bei doLogin kommt (true, Screenname) zurueck,
-        * wenn login erfolgreich war
-        *
-        *
-        */
+      submitButton.maxWidth = Double.MaxValue
+      submitButton.alignment = Pos.Center
+      submitButton.visible = false
       submitButton.onAction = e => {
-        if (codeTextField.getText.size == 0) {
-          //TODO Fehlermeldung anzeigen, dass codefeld nicht leer sein darf
+        if (codeField.getText.size == 0) {
+          warningMessage.visible = true
+
         }
-        val code = codeTextField.getText
-        Controller.setupClient(TwitterLogin.doLogin(code)._2)
+        else {
+          val code = codeField.getText
+          Controller.setupClient(TwitterLogin.doLogin(code)._2)
+        }
       }
-      border.center = new VBox(loginButton, codeTextField, submitButton)
+
+
+      grid.addRow(3, submitButton)
+
+      val warningMessage = new Text("Please enter the login code!")
+      warningMessage.visible = false
+      warningMessage.fill = Red
+      grid.addRow(4, warningMessage)
+
+
+      border.center = grid
       root = border
     }
   }
+
   def exit = ClientView.getStage()
 }
 
